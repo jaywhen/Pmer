37883910b50bd33565f0d5ae4bd4b3c9bc7dc9cc
Xiang Jaywhen <jaywhenxiang@foxmail.com>
2020-10-11 22:37:23 +0800

    add demo

---
+++ NonUI/NonUI/View/RegistView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NonUI.View
+{
+    /// <summary>
+    /// RegistView.xaml 的交互逻辑
+    /// </summary>
+    public partial class RegistView : Window
+    {
+        public RegistView()
+        {
+            InitializeComponent();
+        }
+    }
+}
