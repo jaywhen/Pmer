7d17634099084034c725c283f1fd59a8f6b38ff3
Xiang Jaywhen <jaywhenxiang@foxmail.com>
2020-10-13 22:20:58 +0800

    add noncore: finish the load logic

---
+++ NonCore/NonCore/Views/LoginView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using NonCore.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NonCore.Views
+{
+    /// <summary>
+    /// LoginView.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+            DbCreator db = new DbCreator();
+            db.createDbConnection();
+            bool isLogin = db.Init();
+            // db.createDbConnection();
+            if (!isLogin)
+            {
+                RegistView registView = new RegistView();
+                registView.ShowDialog();
+            } 
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
