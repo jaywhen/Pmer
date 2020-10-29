c955058a9cd453d21789b27df69d57a938745ba3
Xiang Jaywhen <jaywhenxiang@foxmail.com>
2020-10-29 19:17:15 +0800

    add GridSplitter

--- Pmer/Pmer/MainWindow.xaml.cs
+++ Pmer/Pmer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Pmer.ViewModel;
 using System.Windows;
 using Pmer.Views;
+using System.Windows.Input;
 
 namespace Pmer
 {
@@ -12,18 +13,27 @@ namespace Pmer
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         // 只有当isWindowShow为True时，才进行数据绑定
         public MainWindow(bool isWindowShow)
         {
             InitializeComponent();
+            this.MouseDown += (sender, e) =>
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                    this.DragMove();
+            };
             if (isWindowShow)
             {
                 MainWindowModel mainWindowModel = new MainWindowModel();
                 this.DataContext = mainWindowModel;
             }
+            
 
         }
+
+
     }
 }
